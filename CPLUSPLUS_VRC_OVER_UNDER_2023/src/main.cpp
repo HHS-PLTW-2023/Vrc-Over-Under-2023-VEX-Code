cec08ceec405bc671f4c2afb136b71a3488b6391
TRAVIS REX GRAY <TGRAY55197@TRAVISs-MacBook-Air.local>
2023-11-30 12:53:10 -0500

    added more comments for better readability and understandability

--- CPLUSPLUS_VRC_OVER_UNDER_2023/src/main.cpp
+++ CPLUSPLUS_VRC_OVER_UNDER_2023/src/main.cpp
@@ -17,40 +17,53 @@ using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
 vex::brain       Brain;
+
 // define your global instances of motors and other devices here
+//this section assigns motors to ports and assigns motor groups plus the control link for the controller
 // controller 
 vex::controller controller1 = controller(primary);
+//
 
-// drivetrain motors
+// assigning motors to port 2 and 3 for smart drivetrain system
 vex::motor LeftDriveSmart = motor(PORT2, ratio18_1, false);
 vex::motor RightDriveSmart = motor(PORT3, ratio18_1, true);
 vex::drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+//
 
+//assigning motor(s) to ports 9, 10, 19, and 20 for two motor groups used to control the lift arm. 
 // lift arm motor groups
+//upper section (claw side) motor group
 vex::motor one = motor(PORT20, ratio36_1, false);
 vex::motor two = motor(PORT19, ratio36_1, true);
 vex::motor_group upper = motor_group(one, two);
 
+//lower section (base side) motor group
 vex::motor three = motor(PORT9, ratio36_1, true);
 vex::motor four = motor(PORT10, ratio36_1, false);
 vex::motor_group lower = motor_group(three, four);
+//
 
+//assigning motor(s) to port 8 for the claw on top of the lift arm
 //lift arm claw motor
 vex::motor Claw = motor(PORT8, ratio18_1, false);
+// 
 
+// assigning motor(s) to port 4 for the push flaps on the front of the bot 
 //push flap motors
 vex::motor PushArm = motor(PORT4, ratio18_1, false);
+// 
 
-//variables 
+//motor setting tweaking, and function writing to call later in code
+//this is where the majority of the code functions
 //claw states and code
 
+//Not yet implemented //
 int clawOpen()
 {
     Claw.spinFor(forward, 90, degrees);
     Claw.stop(hold);
     return 0;
 }
-
 int clawClosed()
 {
     Claw.spin(reverse);
@@ -58,6 +71,9 @@ int clawClosed()
     Claw.stop(hold);
     return 0;
 }
+//Not yet implemented //
+
+//settings for the upper, lower, and push arm motors
 //motor settings
 int motorSettings()
 {
@@ -77,10 +93,11 @@ int motorSettings()
     return 0;
 } 
 
+//functions for the various subsystems' code
 //subsystem code
 int subsystem()
 {
-    //Push flap arm code
+    //Push flap arm control code
     //push l1 to open arms, pushh l2 to close arms, otherwise stop and hold position
     if (controller1.ButtonL1.pressing()){
         PushArm.spin(forward);
@@ -92,7 +109,8 @@ int subsystem()
         PushArm.stop();
     }
 
-    //Lift Arm code
+    //Lift Arm control code
+    //push r1 to raise, r2 to lower, otherwise stop and hold position
     if (controller1.ButtonR2.pressing()){
         upper.spin(forward); 
         lower.spin(forward);
@@ -112,26 +130,25 @@ int subsystem()
     
 
 
-
+// this is the when started function that runs in main when the thing starts
     int whenStarted1(){ 
         Brain.Screen.printAt( 10, 50, "Is this thing on?" );
-   
+        
+        //allows for user control by making a continuous loop that will run forever (implement killswitch?)
         while(1) {
+
             //call motor settings
             motorSettings();
 
             //call subsystems
             subsystem();
 
-       
-
-
-
             // Allow other tasks to run
             this_thread::sleep_for(10);
         }
     }
 
+// main function that the brain runs to allow the inside code to run 
     int main()
     {
         whenStarted1();
